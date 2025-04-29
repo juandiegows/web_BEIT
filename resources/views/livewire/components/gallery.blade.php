58cfdb892c7a4bed44470b9e13aa3d95b7d28ecb
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2025-04-28 20:17:16 -0500

    gallery

--- resources/views/livewire/components/gallery.blade.php
+++ resources/views/livewire/components/gallery.blade.php
@@ -1,6 +1,6 @@
 <div class="w-[90%] lg:w-[80%] max-w-7xl mx-auto grid-flow-dense grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
     @foreach($images as $image)
-        <div class="@if($image['size'] === 'horizontal') sm:col-span-2 md:col-span-2 xl:col-span-2
+        <div class="@if($image['size'] === 'horizontal') sm:col-span-2 md:col-span-2 xl:col-span-2por 
                      @elseif($image['size'] === 'vertical') row-span-2 
                      @endif cursor-pointer">
             <img  src="{{ $image['url'] }}" 
